b3dcbfb2b611b2f18115647048b9736cbe75a1c4
kioli <kioli85@gmail.com>
2017-09-25 13:01:45 +0200

    Add comments

--- app/src/main/java/kioli/rx/QuoteContract.kt
+++ app/src/main/java/kioli/rx/QuoteContract.kt
@@ -8,16 +8,39 @@ import kioli.rx.mvp.IView
 internal interface QuoteContract {
 
     interface QuoteView : IView {
+        /**
+         * Return the result of asking for a quote to the calling QuoteView
+         *
+         * @param quote the requested quote
+         */
         fun returnResultQuote(quote: Quote?)
+
+        /**
+         * Show the loading screen in the QuoteView
+         */
         fun showLoading()
+
+        /**
+         * Hide the loading screen in the QuoteView
+         */
         fun hideLoading()
     }
 
     interface QuotePresenter : IPresenter<QuoteView> {
+        /**
+         * Get a quote to present to the QuoteView
+         *
+         * @param forceNew true if the quote to get should be a new one, false otherwise
+         */
         fun getQuote(forceNew: Boolean)
     }
 
     interface QuoteModel {
+        /**
+         * Retrieve a quote wrapping it in a reactive flowable
+         *
+         * @return a flowable containing the retrieved quote
+         */
         fun fetchQuote(): Flowable<Quote>
     }
 }
